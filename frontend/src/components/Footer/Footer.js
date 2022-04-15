86c4b994e0b7e3a6a0ecaa755880b4189022e89b
Salih <salih1010@abv.bg>
2022-04-15 16:44:33 +0300

    Made a simple footer

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,7 +1,26 @@
 import React from 'react'
 const Footer = () => {
   return (
-    <div>Footer
+    <div className='footer'>
+      <div>
+        <h3>Help and information</h3>
+        <p>Help</p>
+        <p>Track oreder</p>
+        <p>Delivery and returns</p>
+      </div>
+      <div>
+        <h3>About asos</h3>
+        <p>About us</p>
+        <p>Career at Asos</p>
+        <p>Corpoorate responsibility</p>
+        <p>Investor's site</p>
+      </div>
+      <div>
+        <h3>More from asos</h3>
+        <p>Mobile and Asos apps</p>
+        <p>Assos marketplace</p>
+        <p>Gift vouchers</p>
+      </div>
     </div>
   )
 }

438d11dda4a95eb571033a178406081c669a0181
Salih <salih1010@abv.bg>
2022-04-15 13:09:27 +0300

    Created descktop version of header

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -3,8 +3,20 @@ import { Link } from 'react-router-dom';
 const Header = () =>{
     return(
         <div className='header'>
-            Header
-            <Link to='/test'>Test</Link>
+            <img className='logo' src='images/logo.jpg'/>
+            <div className='categorie'>
+                <p>Men</p>
+            </div>
+            <div className='categorie'>
+                <p>Women</p>
+            </div>
+            <div className='search-bar-holder'>
+                <input className='search-bar' placeholder='search for items and brands'/>
+                <img src='images/search.png' className='search-icon'/>
+            </div>
+            <img src='images/user.png'/>
+            <img src='images/love.png'/>
+            <img src='images/shopping-bag.png'/>
         </div>
     )
 }

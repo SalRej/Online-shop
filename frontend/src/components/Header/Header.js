c92d84d6b8a693e3eba7bdf6331b6bdbcd17c7a0
Salih <salih1010@abv.bg>
2022-04-15 16:34:39 +0300

    Made the header responsive , minor change on style for descktop header

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React ,{useState,useEffect} from 'react';
 import { Link } from 'react-router-dom';
 const Header = () =>{
+
+    const [screenWidth,setScreenWidth] = useState(0);
+
+    useEffect(()=>{
+        window.addEventListener("resize",()=>{
+            setScreenWidth(window.innerWidth);
+        })
+    },[screenWidth]);
     return(
         <div className='header'>
+            {(window.innerWidth <= 1000)?<img className='menu' src='images/menu.png'/>:<></>}
             <img className='logo' src='images/logo.jpg'/>
             <div className='categorie'>
                 <p>Men</p>
@@ -14,6 +23,7 @@ const Header = () =>{
                 <input className='search-bar' placeholder='search for items and brands'/>
                 <img src='images/search.png' className='search-icon'/>
             </div>
+            {(window.innerWidth <= 700) && <img className='search-icon' src='images/search-mobile.png' />}
             <img src='images/user.png'/>
             <img src='images/love.png'/>
             <img src='images/shopping-bag.png'/>

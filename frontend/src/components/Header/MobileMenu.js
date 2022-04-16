78264dd0264c69278f818ea4aaa09a69ae2340e7
Salih <salih1010@abv.bg>
2022-04-16 23:50:00 +0300

    Changed mobile menu to sho categories instead to switch to categorie page

--- frontend/src/components/Header/MobileMenu.js
+++ frontend/src/components/Header/MobileMenu.js
@@ -1,24 +1,41 @@
-import React from 'react'
+import React ,{useEffect ,useState}from 'react'
 import { Link } from 'react-router-dom';
 function MobileMenu(props) {
+
+    const [isLoaded,setIsLoaded] = useState(false);
+    const [mainCategorie,setMaincategorie] = useState('mens');
+    const [categories,setCategories] = useState();
+    useEffect(()=>{
+        fetch(`http://localhost:5000/getCategorie?categorie=${mainCategorie}`)
+        .then(res=>res.json())
+        .then(data=>{
+            setCategories(data[0]);
+            setIsLoaded(true);
+            console.log(data[0]);
+        })
+    },[mainCategorie]);
 
     return (
     <div className='mobile-menu'>
 
         <div className='categorie-holder'>
-            <Link to='/mens'>
-                <div onClick={()=>{props.toogleShow()}} className='mobile-categorie'>
-                    <p>
-                        Men
-                    </p>
-                </div>
-            </Link>
-            <Link to='/womens'>
-                <div onClick={()=>{props.toogleShow()}} className='mobile-categorie'>
-                    <p>Women</p>
-                </div>
-            </Link>
+            <div onClick={()=>{setMaincategorie('mens')}}className='mobile-categorie'>
+                <p>
+                    Men
+                </p>
+            </div>
+            <div onClick={()=>{setMaincategorie('womens')}} className='mobile-categorie'>
+                <p>Women</p>
+            </div>
         </div>
+        {isLoaded===true &&  categories.categories.map(item=>{
+            return (
+                <div className='categorie-info'>
+                    <p>{item.name}</p>
+                    <img src={`images/${item.image}`}/>
+                </div>
+            )
+        })}
     </div>
     )
 }

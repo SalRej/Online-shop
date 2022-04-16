eb579a171480fc7baf650822396c2e94384cb133
Salih <salih1010@abv.bg>
2022-04-16 13:17:10 +0300

    Made getSales route to return 6 random products

--- backend/routes/getSales.js
+++ backend/routes/getSales.js
@@ -5,14 +5,29 @@ const Product = require('../models/Product');
 router.get('/',async (req,res)=>{
 
     //queryis 6 products on sales , takes 1 image and the name of the product and sends i to the home page
-    const result = await Product.find({c_isSale:true},{name:1,image_groups:1,id:1}).limit(6);
-    const data = result.map((item)=>{
-        return {
-            name:item.name,
-            imageLink:item.image_groups[0].images[0].link,
-            id:item.id
+    const result = await Product.find({c_isSale:true},{name:1,image_groups:1,id:1});
+    const lenght = result.length;
+
+    let set;
+    do{
+        const randomIndexes = Array(6).fill().map(() => Math.round(Math.random() * lenght));
+        set = new Set(randomIndexes);
+        console.log(set);
+
+    }while(set.size!=6)
+    
+    const data = [];
+    result.forEach((item,index)=>{
+        if(set.has(index)){
+            console.log("ima" + index);
+            data.push({
+                name:item.name,
+                imageLink:item.image_groups[0].images[0].link,
+                id:item.id
+            })
         }
     })
+    console.log(data);
     res.send(JSON.stringify({data:data}));
 })
 

9967939e310b4fbca4b796fd6bafa20d4298e818
Salih <salih1010@abv.bg>
2022-04-18 12:34:02 +0300

    Modified getProducts to send min and max price of item

--- backend/routes/getProducts.js
+++ backend/routes/getProducts.js
@@ -5,8 +5,19 @@ const Categories = require('../models/Categories');
 router.get('/',async (req,res)=>{
     const queryParam = req.query.productsType;
     const result = await Products.find({primary_category_id:queryParam});
-    
-    res.send(JSON.stringify({data:result}));
+    let min = result[0].price;
+    let max = result[0].price;
+
+    //find min and max price 
+    result.forEach(item=>{
+        if(item.price < min){
+            min = item.price;
+        }
+        if(item.price > max){
+            max = item.price;
+        }
+    })
+    res.send(JSON.stringify({data:result,minPrice:min,maxPrice:max}));
 })
 
 module.exports =router;

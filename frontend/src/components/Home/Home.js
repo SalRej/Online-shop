fef9bb3be2dea656c226a6df26c26850eb3f17ba
Salih <salih1010@abv.bg>
2022-04-22 11:07:21 +0300

    Added new elements and style to home page

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -15,26 +15,44 @@ function Home() {
     },[])
 
     return (
-        <div className='home-page'>
-            <h2>MID SESSSION SALE</h2>
-            <h1>NOW UP TO 40% OFF</h1>
-            <div className='sales-card-holder'>
-                {
-                isLoaded &&  products.map((item)=>{
-                        return(
-                            <Link to='/test' key={item.id}>
-                                <div className='sales-card'>
-                                    <div className='image-container'>
-                                        <img src={`images/${item.imageLink}`}/>   
+        <div class='home-page'>
+            <div className='main-screen'>
+                <h2>MID SESSSION SALE</h2>
+                <h1>NOW UP TO 40% OFF</h1>
+                <div className='sales-card-holder'>
+                    {
+                    isLoaded &&  products.map((item)=>{
+                            return(
+                                <Link to='/test' key={item.id}>
+                                    <div className='sales-card'>
+                                        <div className='image-container'>
+                                            <img src={`images/${item.imageLink}`}/>   
+                                        </div>
+                                        <p>{item.name}</p>
                                     </div>
-                                    <p>{item.name}</p>
-                                </div>
-                            </Link>
-                        )
-                    })
-                } 
+                                </Link>
+                            )
+                        })
+                    } 
+                </div>
+                <button>VIEW ALL</button>
+    
             </div>
-            <button>VIEW ALL</button>
+            <img class='big-image' src='/images/home/man-fashion-1.jpg'/>
+            <div className='smaller-img-holder'>
+                <img src='/images/home/man-fashion-2.jpg'/>
+                <img src='/images/home/man-fashion-3.jpg'/>
+            </div>
+
+            <div className='bottom'>
+                <img src='/images/facebook.png'/>
+                <img src='/images/instagram.png'/>
+                <img src='/images/snapchat.png'/>
+                <p>|</p>
+                <img src='/images/visa.png'/>
+                <img src='/images/master-card.png'/>
+                <img src='/images/paypal.png'/>
+            </div>
         </div>
     )
 }

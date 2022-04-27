039208c7c64151498276334a104b1e5cda60b31f
Salih <salih1010@abv.bg>
2022-04-27 21:12:51 +0300

    Made color and size not to show if there are non in product description

--- frontend/src/components/ProductsDescription/DataSection.jsx
+++ frontend/src/components/ProductsDescription/DataSection.jsx
@@ -5,52 +5,61 @@ function DataSection(props) {
     <div className='data-section'>
         <p className='item-number'>Item No.{props.product.id}</p>
         <p className='title'>{props.product.page_title}</p>
-        <p className='price'>{props.product.price}{props.product.currency}</p>
-        <h3>Color</h3>
-        <div className='holder my-checkbox'>
-            {
-                props.swatchImages.map(item=>{
-                    if(typeof(item.variation_value)!='undefined'){
-                        return item.images.map((image,index)=>{
+        <p className='price'>{props.product.price}<span>{props.product.currency}</span></p>
+        {   props.swatchImages.length>0 && 
+            <React.Fragment>
+                <h3>Color</h3>
+                <div className='holder my-checkbox'>
+                    {
+                        props.swatchImages.map(item=>{
+                            if(typeof(item.variation_value)!='undefined'){
+                                return item.images.map((image,index)=>{
+                                    return(
+                                        <React.Fragment>
+                                            <input
+                                                id={item.variation_value} type='radio' 
+                                                value={item.variation_value} name='color'
+                                            />
+                                            <label htmlFor={item.variation_value} className='animate__animated animate__flipInX'>
+                                                <img 
+                                                    onClick={()=>{props.changeVariation(item.variation_value)}}
+                                                    key={index}
+                                                    src={`/images/${image.link}`}
+                                                />
+                                            </label>
+                                        </React.Fragment>
+                                    )
+                                })
+                            }
+                        })
+                    }
+                </div>
+            </React.Fragment>
+        }
+        {
+            typeof(props.product.variation_attributes[1])!='undefined' &&
+             <React.Fragment>
+                <h3>Size</h3>
+                <div className='holder my-checkbox'>
+                    {
+                       
+                        props.product.variation_attributes[1].values.map(item=>{
                             return(
                                 <React.Fragment>
                                     <input
-                                        id={item.variation_value} type='radio' 
-                                        value={item.variation_value} name='color'
+                                        id={item.name} type='radio' 
+                                        value={item.value} name='size'
                                     />
-                                    <label htmlFor={item.variation_value} className='animate__animated animate__flipInX'>
-                                        <img 
-                                            onClick={()=>{props.changeVariation(item.variation_value)}}
-                                            key={index}
-                                            src={`/images/${image.link}`}
-                                        />
+                                    <label htmlFor={item.name} className='animate__animated animate__flipInX'>
+                                        <div>{item.name}</div>
                                     </label>
                                 </React.Fragment>
                             )
                         })
                     }
-                })
-            }
-        </div>
-        <h3>Size</h3>
-        <div className='holder my-checkbox'>
-            {
-                typeof(props.product.variation_attributes[1])!='undefined' &&
-                props.product.variation_attributes[1].values.map(item=>{
-                    return(
-                        <React.Fragment>
-                            <input
-                                id={item.name} type='radio' 
-                                value={item.value} name='size'
-                            />
-                            <label htmlFor={item.name} className='animate__animated animate__flipInX'>
-                                <div>{item.name}</div>
-                            </label>
-                        </React.Fragment>
-                    )
-                })
-            }
-        </div>
+                </div>
+             </React.Fragment>
+        }
         <button>Add to cart</button>
     </div>
   )
